721a6d4e3b4cf1af42c45d3b1fdfb41dc5447e3b
kkaess <kwkaess@ucdavis.edu>
2018-12-14 20:34:30 -0800

    Put in most of the refactoring to sort by date if I want to

--- printFitsComments.py
+++ printFitsComments.py
@@ -8,13 +8,36 @@
 
 import sys
 from astropy.io import fits
+from datetime import datetime
 
-for fn in sys.argv[1:]:
-	with fits.open(fn) as f:
-		print(fn)
-		if 'COMMENT' in f[0].header.keys():
-			print(f[0].header['COMMENT'])
-		print()
+def printFitsComment(fileList,orderByTimestamp=False):
+	stringListList = []
+	for filename in fileList:
+		with fits.open(filename) as f:
+			stringList = [filename]
+			if 'COMMENT' in f[0].header.keys():
+				stringList.append(f[0].header['COMMENT'])
+			else:
+				stringList.append('')
+			if 'DATE' in f[0].header.keys():
+				stringList.append(datetime.strptime(f[0].header['DATE'],"%Y-%m-%dT%H:%M:%S")
+			else:
+				stringList.append(datetime.now()))
+		stringListList.append(stringList)
+	if orderByTimestamp:
+		stringListList.sort(key = lamda tup: tup[1])
+			
+	#Insert printout stuff
+	
+
+if __name__ = "__main__":
+
+	for fn in sys.argv[1:]:
+		with fits.open(fn) as f:
+			print(fn)
+			if 'COMMENT' in f[0].header.keys():
+				print(f[0].header['COMMENT'])
+			print()
 
  
 
