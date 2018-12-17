1dc5adcda76f7ece05284e26a6b79360c454cdb8
kkaess <kwkaess@ucdavis.edu>
2018-12-17 10:56:52 -0800

    Put in printout code
    Finished adding functionality for now. Haven't tested yet, so may see
    another commit before long.

--- printFitsComments.py
+++ printFitsComments.py
@@ -1,7 +1,7 @@
 # printFitsComments.py
 
 # usage: python3 printFitsComments.py <space separated list of fits files with sufficient path data to open directly>
-# effect: prints 1) the filename passed
+# effect: prints 1) each filename passed
 # 		 2) the 'COMMENT' field of the fits header of the first HDU in the file associated with that filename
 #			*if it exists*
 # fails on: no header(?!), no hdu, no fits file at any location specified.
@@ -11,23 +11,29 @@ from astropy.io import fits
 from datetime import datetime
 
 def printFitsComment(fileList,orderByTimestamp=False):
-	stringListList = []
+	stringListList = [] #container to store the information
 	for filename in fileList:
 		with fits.open(filename) as f:
-			stringList = [filename]
+			stringList = [filename] #enter filename as first element in entry
 			if 'COMMENT' in f[0].header.keys():
-				stringList.append(f[0].header['COMMENT'])
+				stringList.append(f[0].header['COMMENT']) #enter COMMENT as second element in entry
 			else:
 				stringList.append('')
 			if 'DATE' in f[0].header.keys():
-				stringList.append(datetime.strptime(f[0].header['DATE'],"%Y-%m-%dT%H:%M:%S")
+				# parse header DATE field and enter as third element in entry
+				stringList.append(datetime.strptime(f[0].header['DATE'],"%Y-%m-%dT%H:%M:%S"))
 			else:
-				stringList.append(datetime.now()))
+				stringList.append(datetime.today()+datetime.day)
 		stringListList.append(stringList)
 	if orderByTimestamp:
-		stringListList.sort(key = lamda tup: tup[1])
+		stringListList.sort(key = lamda x: x[2]) #sorts by the date
 			
-	#Insert printout stuff
+	#Currently outputs as plaintext - may update later to output in markdown or similar
+	for entry in stringListList:
+		
+		print(entry[0])
+		print('date: ' + str(entry[2]))
+		print("\n"+entry[1]+"\n\n")
 	
 
 if __name__ = "__main__":
