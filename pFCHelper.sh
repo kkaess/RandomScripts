8a74222c236ae3bf202628739d1a4dbe72affcf2
kkaess <kwkaess@ucdavis.edu>
2018-12-17 11:28:55 -0800

    Debugged and added helper script
    printFitsComents.py now works
    pFCHelper.sh is a bash script, specific to the structure of the file
    output for my project, that automates generating a list of files for me.

---
+++ pFCHelper.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# A bash script to automate comment extraction from a 
+# parent folder that contains AIDA run folders and a SingleScans folder.  
+# This is unique to AIDA, so only useful for our project.
+
+# Usage: pFCHelper.sh <1: path of data folder> <2: (optional) path of printFitsComment.py>
+
+# both parameters should be terminated with a / if used.
+
+python3 $2printFitsComments.py $(ls $1*/Picture_1/Frame_1.fits $1SingleScans/*.fits)
